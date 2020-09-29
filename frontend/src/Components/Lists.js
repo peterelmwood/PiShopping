7e65d9b35ad32b8a398aad0799734b219272f1e6
peterelmwood <peterelmwood@gmail.com>
2020-09-28 22:49:46 -0500

    getting it a little closer

--- frontend/src/Components/Lists.js
+++ frontend/src/Components/Lists.js
@@ -21,24 +21,24 @@ import Switch from '@material-ui/core/Switch';
 import DeleteIcon from '@material-ui/icons/Delete';
 import FilterListIcon from '@material-ui/icons/FilterList';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, count) {
+  return { name, count };
 }
 
 const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
+  createData('Cupcake', 4),
+  createData('Donut', 2),
+  createData('Eclair', 7),
+  createData('Frozen yoghurt', 5),
+  createData('Gingerbread', 1),
+  createData('Honeycomb', 11),
+  createData('Ice cream sandwich', 9),
+  createData('Jelly Bean', 21),
+  createData('KitKat', 25),
+  createData('Lollipop', 72),
+  createData('Marshmallow', 300),
+  createData('Nougat', 3),
+  createData('Oreo', 24),
 ];
 
 function descendingComparator(a, b, orderBy) {
@@ -69,10 +69,7 @@ function stableSort(array, comparator) {
 
 const headCells = [
   { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
-  { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-  { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-  { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-  { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
+  { id: 'count', numeric: true, disablePadding: false, label: 'Count' },
 ];
 
 function EnhancedTableHead(props) {
@@ -320,10 +317,7 @@ export default function Lists() {
                       <TableCell component="th" id={labelId} scope="row" padding="none">
                         {row.name}
                       </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
+                      <TableCell align="right">{row.count}</TableCell>
                     </TableRow>
                   );
                 })}
